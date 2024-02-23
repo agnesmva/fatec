8f68d2fa85856570c3cea62e48893f34ec06d71e
Agnes Maria <agnesmorganav@gmail.com>
2024-02-23 13:19:11 -0300

    Exercício II

---
+++ first/algoritmos_e_logica/aula_II/exercio_II.c
@@ -0,0 +1,15 @@
+#include <stdio.h> //Elabore um programa em C que leia as medidas de um terreno retangular(comprimento e largura) calcule e mostre qual é o perímetro e qual a área do terreno.
+int main()
+{
+    float comp, larg, peri, area;
+    printf("\nQual o comprimento do seu terreno?");
+    scanf("%f", & comp);
+    printf("\nQual a largura do seu terreno?");
+    scanf("%f", & larg);
+    peri = comp + larg;
+    area = comp * larg;
+    printf("\nAqui está o perímetro do seu terreno: %2.f que possui area de: %2.f\n", peri, area);
+
+    return 0;
+
+}
