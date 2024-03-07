617f5100f8a860017fdfbdf52250785cd3db6a35
Agnes <agnesmorganav@gmail.com>
2024-03-07 19:45:10 -0300

    correção ex_11

--- first/algoritmos_e_logica/aula_03/ex_11.c
+++ first/algoritmos_e_logica/aula_03/ex_11.c
@@ -9,7 +9,7 @@ int main()
     int ano;
     printf("\nVamos verificar se esse ano é bissexto? Insira um ano: ");
     scanf("%d", & ano);
-    if (((ano %400) == 0) || (((ano%4) == 0)) && (ano%100)!=0)
+    if (((ano %400) == 0) || (((ano%4) == 0)) && !(ano%100)==0)
     {
         printf("O ano inserido é bissexto!!");
     }
